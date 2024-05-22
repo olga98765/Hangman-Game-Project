2ab6f5c1622e158a64f4be263066052e469dcee0
ola <o.monko@o2.pl>
2024-05-22 11:26:15 +0200

    Manu

--- InputHelper.java
+++ InputHelper.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 
 public class InputHelper {
 
-
     static Scanner scanner = new Scanner(System.in);
 
+    public static String readLine() {
+        return scanner.nextLine();
+    }
 
-public static String readLine() {
-    return scanner.nextLine();
-}
-
-public static void close() {
-    scanner.close();
-}
-
+    public static void close() {
+        scanner.close();
+    }
 
 }
--- Menu.java
+++ Menu.java
@@ -7,29 +7,43 @@ public void displayMenu() {
 
 System.out.println("\nWybierz opcję\n\n1.Graj\n2. Dodaj słowo\n3. Usuń słowo\n4. Edytuj słowo\n5. Pokaż statystyki\n6. Pokaż słowa w grze\n7. Wyjdź\n");
 
+String option = InputHelper.readLine();
 
-
-switch () {
+switch (option) {
     case "1":
-        playGame();
+
+    System.out.println("1");
+        // playGame();
         break;
     case "2":
-        addWord();
+    System.out.println("2");
+
+        // addWord();
         break;
     case "3":
-        removeWord();
+    System.out.println("3");
+
+        // removeWord();
         break;
     case "4":
-        editWordByNumber();
+    System.out.println("4");
+
+        // editWordByNumber();
         break;
     case "5":
-        showStatistics();
+    System.out.println("5");
+
+        // showStatistics();
         break;
     case "6":
-        showWordDatabase();
+    System.out.println("6");
+
+        // showWordDatabase();
         break;
     case "7":
-        close();
+    System.out.println("7");
+
+        // close();
         break;
     default:
         System.out.println("Niepoprawna opcja. Spróbuj ponownie.");
